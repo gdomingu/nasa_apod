8526ac8d2f0b1c531d7fde62db3dd0aa119cc975
Gabe D <gpad88@gmail.com>
2016-04-07 08:32:40 -0700

    cleaned up search method

--- lib/nasa_apod/client.rb
+++ lib/nasa_apod/client.rb
@@ -3,17 +3,17 @@ module NasaApod
   DEFAULT_URL = 'https://api.nasa.gov/planetary/apod'
   
   class Client
-    attr_reader :api_key, :date, :list_concepts, :hd
+    attr_reader :api_key, :date, :concept_tags, :hd
 
     def date=(date)
       @date = parse_date(date)
     end
 
-    def list_concepts=(list_concepts)
-      if list_concepts.nil? || list_concepts.blank?
-        @list_concepts = false
+    def concept_tags=(concept_tags)
+      if concept_tags.nil? || concept_tags.blank?
+        @concept_tags = false
       else
-        @list_concepts = list_concepts
+        @concept_tags = concept_tags
       end
     end
 
@@ -28,7 +28,7 @@ module NasaApod
     def initialize(options={})
       @api_key = options[:api_key] || "DEMO_KEY"
       self.date = options[:date]
-      self.list_concepts = options[:list_concepts]
+      self.concept_tags = options[:concept_tags]
       self.hd = options[:hd]
     end
 
@@ -44,18 +44,24 @@ module NasaApod
     # @return [NasaApod::SearchResults] Return APOD post for a specified date.
     def search(options={})
       self.date = options[:date] || date
-      @list_concepts = options[:list_concepts] || list_concepts
+      @concept_tags = options[:concept_tags] || concept_tags
       @hd = options[:hd] || hd
-      response = HTTParty.get("https://api.nasa.gov/planetary/apod?api_key=#{api_key}&date=#{date}&concept_tags=#{list_concepts}&hd=#{hd}")
+      response = HTTParty.get(DEFAULT_URL, { query: attributes })
       handle_response(response)
     end
 
     def random_post(options={})
       date = rand(Date.parse("1995-06-16")..Date.today)
-      search(:date => date)
+      search(date: date)
     end
 
     alias_method :wormhole, :random_post
+
+    def attributes
+      instance_variables.each_with_object(Hash.new) do  |var,hash| 
+        hash[var.to_s.delete("@")] = instance_variable_get(var)
+      end
+    end
 
     private
 
@@ -65,14 +71,6 @@ module NasaApod
       else
         NasaApod::Error.new(response)
       end
-    end
-
-    def write_attrs(attributes)
-      @concepts = attributes["concepts"]
-      @url = attributes["url"]
-      @media_type = attributes["media_type"]
-      @explanation = attributes["explanation"] 
-      @title = attributes["title"]
     end
 
     def parse_date(date)
